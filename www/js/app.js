330144157ef1874fedfc07c3e9a82f6d0ec8d2aa
firepick1 (Chromebook Ubuntu) <karl@firepick.org>
2014-05-12 14:56:56 -0700

    angularjs

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('FireREST', [
+  'ngRoute',
+  'FireREST.bootstrap',
+//  'FireREST.filters',
+//  'FireREST.services',
+//  'FireREST.directives',
+  'FireREST.controllers'
+]).
+config(['$routeProvider', function($routeProvider) {
+//  $routeProvider.when('/status', {templateUrl: 'partials/status.html', controller: 'StatusCtrl'});
+//  $routeProvider.when('/spindle', {templateUrl: 'partials/spindle.html', controller: 'SpindleCtrl'});
+//  $routeProvider.when('/camera', {templateUrl: 'partials/camera.html', controller: 'CameraCtrl'});
+//  $routeProvider.when('/move', {templateUrl: 'partials/move.html', controller: 'MoveCtrl'});
+//  $routeProvider.when('/configure', {templateUrl: 'partials/configure.html', controller: 'ConfigureCtrl'});
+//  $routeProvider.otherwise({redirectTo: '/status'});
+}]);
---
+++ www/js/controllers.js
@@ -0,0 +1,40 @@
+'use strict';
+var bootstrap = angular.module('FireREST.bootstrap', ['ui.bootstrap']);
+
+var controllers = angular.module('FireREST.controllers', []);
+
+controllers.controller('MainCtrl', ['$scope','$location',
+  function(scope, location) {
+    scope.imageInstances = {};
+    scope.imageLarge = {};
+
+    scope.image_url = function(image) {
+      var t = scope.imageInstances[image] || 0;
+      return "cv/1/" + image + "?t=" + t;
+    };
+    scope.image_class = function(image) {
+      var isLarge = scope.imageLarge[image] || false;
+      return isLarge ? "fr-img-lg" : "fr-img-sm";
+    }
+    scope.image_click = function(image) {
+      var isLarge = scope.imageLarge[image] || false;
+      scope.imageLarge[image] = !isLarge;
+    };
+    scope.image_GET = function(image) {
+      scope.imageInstances[image] = Math.random();
+    };
+    scope.action_GET = function(action) {
+    console.log("action_GET");
+	$.ajax({
+	  url: "cv/1/gray/cve/calc-offset/" + action + ".json",
+	  data: { r: Math.random() },
+	  success: function( data ) {
+	    var text = JSON.stringify(data);
+	    $( "#" + action + "-json" ).html( text );
+	  },
+	  error: function( jqXHR, ex) {
+	    console.log("ERROR: " + JSON.stringify(jqXHR) + " EX:" + JSON.stringify(ex));
+	  }
+	});
+      }
+}]);
