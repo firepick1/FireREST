6c4db006573378b2b27654a257a5ca2b1d5f4c4c
firepick1 (Chromebook Ubuntu) <karl@firepick.org>
2014-05-12 17:02:00 -0700

    form

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,13 +5,25 @@ var controllers = angular.module('FireREST.controllers', []);
 
 controllers.controller('MainCtrl', ['$scope','$location',
   function(scope, location) {
+    scope.server = "localhost";
+    scope.port = 8001;
+    scope.service = "/firerest";
+    scope.camera = 1;
+    scope.profile = "gray";
+    scope.cve = "calc-offset";
+
     scope.imageInstances = {};
     scope.imageLarge = {};
 
+    scope.camera_url = function() {
+      return "http://" + scope.server + ":"  + scope.port + scope.service + "/cv/" + scope.camera + "/";
+    };
+
     scope.image_url = function(image) {
       var t = scope.imageInstances[image] || 0;
-      return "cv/1/" + image + "?t=" + t;
+      return scope.camera_url() + image + "?t=" + t;
     };
+
     scope.image_class = function(image) {
       var isLarge = scope.imageLarge[image] || false;
       return isLarge ? "fr-img-lg" : "fr-img-sm";
@@ -23,10 +35,14 @@ controllers.controller('MainCtrl', ['$scope','$location',
     scope.image_GET = function(image) {
       scope.imageInstances[image] = Math.random();
     };
+    
+    scope.action_url = function(action) {
+      return scope.camera_url() + scope.profile + "/cve/" + scope.cve + "/" + action + ".json";
+    };
     scope.action_GET = function(action) {
 	$( "#" + action + "-json" ).html( "..." );
 	$.ajax({
-	  url: "cv/1/gray/cve/calc-offset/" + action + ".json",
+	  url: scope.action_url(action),
 	  data: { r: Math.random() },
 	  success: function( data ) {
 	    var text = JSON.stringify(data);
