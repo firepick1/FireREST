506b3c4562f1e92c499c34710e782071b6d7b404
firepick1 (Chromebook Ubuntu) <karl@firepick.org>
2014-05-19 16:52:13 -0700

    use config.json for all resource references

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,24 +5,25 @@ var controllers = angular.module('FireREST.controllers', []);
 
 controllers.controller('MainCtrl', ['$scope','$location', 'BackgroundThread',
   function(scope, location, bg) {
-    scope.content_source = "";
-    scope.server = location.host() || "unknownhost";
-    scope.port = location.port() || "unknownport";
-    scope.service = "/firerest";
-    scope.transmit_enabled = true;
-    scope.camera = 1;
-    scope.image_update = true;
-    scope.resource_update = true;
+    scope.cv = {
+      "resources":['save', 'process'],
+      "image":[],
+      "server":location.host() || "unknownhost",
+      "port":location.port() || "unknownport",
+      "service": "/firerest",
+      "collapse": {"camera":true, "cve":true, "service":true},
+      "image_instances":{},
+      "image_large":{}
+      };
+    scope.transmit_enabled = false;
     scope.transmit = 1; // 0:error, 1:idle, >1:active-network-requests
-    scope.profile = "gray";
-    scope.cve = "calc-offset";
 
     scope.service_url = function() {
-      var port = scope.port === "" ? "" : (":" + scope.port);
-      return "http://" + scope.server + port + scope.service;
+      var port = scope.cv.port === "" ? "" : (":" + scope.cv.port);
+      return "http://" + scope.cv.server + port + scope.cv.service;
     };
     scope.camera_url = function() {
-      return scope.service_url() + "/cv/" + scope.camera + "/";
+      return scope.service_url() + "/cv/" + scope.cv.camera_name + "/";
     };
 
     scope.transmit_status = function() {
@@ -53,115 +54,100 @@ controllers.controller('MainCtrl', ['$scope','$location', 'BackgroundThread',
       }
     }
     
+    scope.config_url = function() {
+      return scope.service_url() + "/config.json";
+    }
+    scope.cve_names = function() {
+      var camera = scope.cv.camera_name && scope.config.cv.camera_map[scope.cv.camera_name];
+      var profile = camera && camera.profile_map[scope.cv.profile_name];
+      return profile && profile.cve_names || [];
+    }
     scope.config_load = function() {
       scope.transmit_start();
       $.ajax({
-	url: scope.service_url() + "/config.json",
+	url: scope.config_url(),
 	data: { },
 	success: function( data ) {
 	  scope.$apply(function(){
 	    scope.transmit_end(true);
 	    console.log(JSON.stringify(data));
 	    scope.config = data;
-	    scope.cve_map = Object.keys(data.cv.cve_map);
-	    scope.camera_map = Object.keys(data.cv.camera_map); 
+	    if (typeof scope.config.cv === 'object') {
+	      var cv = scope.cv;
+	      cv.camera_names = Object.keys(scope.config.cv.camera_map); 
+	      cv.camera_name = cv.camera_names[0];
+	      cv.image = ['monitor.jpg'];
+	      cv.profile_names = cv.camera_name && Object.keys(scope.config.cv.camera_map[cv.camera_name].profile_map);
+	      cv.profile_name = cv.profile_names[0];
+	      cv.cve_name = scope.cve_names()[0] || "no-CVE";
+	    }
 	  });
 	},
 	error: function( jqXHR, ex) {
 	  console.error("config_load() ex:" + ex + "," + JSON.stringify(jqXHR));
 	  scope.$apply(function(){
 	    scope.transmit_end(false);
-	    scope.cve_map = ["CVE n/a"];
-	    scope.camera_map = ["camera n/a"];
+	    scope.cv.camera_names = ["camera n/a"];
 	  });
 	}
       });
     };
     scope.config_load();
 
-    scope.collapse = {"camera":true, "cve":true, "service":true};
     scope.collapse_icon = function(value) {
       return "glyphicon fr-collapse-icon " +
-        (scope.collapse[value] ? "glyphicon-wrench" : "glyphicon-wrench");
-    }
-    scope.collapse_class = function(value) {
-      return scope.collapse[value] ? "fr-hide" : "fr-show";
+        (scope.cv.collapse[value] ? "glyphicon-wrench" : "glyphicon-wrench");
     }
     scope.collapse_toggle = function(value) {
-      scope.collapse[value] = !scope.collapse[value];
+      scope.cv.collapse[value] = !scope.cv.collapse[value];
     }
 
-    scope.check_content_source = function() {
-      scope.content_source = "...";
-      scope.transmit_start();
-      $.ajax({
-	url: scope.service_url() + "/content_source",
-	data: { r: Math.floor(Math.random()*1000000) },
-	success: function( data ) {
-	  scope.$apply(function(){
-	    scope.transmit_end(true);
-	    scope.content_source = (data || "").trim();
-	  });
-	},
-	error: function( data ) {
-	  scope.$apply(function(){
-	    scope.transmit_end(false);
-	  });
-	}
-	  
-      });
-    };
     scope.clear_results = function() {
       scope.resource_response = {};
       scope.resource_classname = {};
-      scope.check_content_source();
     };
     scope.clear_results();
 
-    scope.show_image = ['monitor.jpg'];
-    scope.image_instances = {};
-    scope.imageLarge = {};
     scope.image_path = function(image) {
-      var r = scope.image_instances[image] || 0;
+      var r = scope.cv.image_instances[image] || 0;
       if (image === 'saved.png') {
-	return "/cv/" + scope.camera + "/" + scope.profile + "/cve/" + scope.cve + "/" + image + "?r=" + r;
+	return "/cv/" + scope.cv.camera_name + "/" + scope.cv.profile_name + "/cve/" + scope.cv.cve_name + "/" + image + "?r=" + r;
       } else {
-	return "/cv/" + scope.camera + "/" + image + "?r=" + r;
+	return "/cv/" + scope.cv.camera_name + "/" + image + "?r=" + r;
       }
     };
     scope.image_url = function(image) {
-      var r = scope.image_instances[image] || 0;
+      var r = scope.cv.image_instances[image] || 0;
       if (image === 'saved.png') {
-	return scope.camera_url() + scope.profile + "/cve/" + scope.cve + "/" + image + "?r=" + r;
+	return scope.camera_url() + scope.cv.profile_name + "/cve/" + scope.cv.cve_name + "/" + image + "?r=" + r;
       } else {
 	return scope.camera_url() + image + "?r=" + r;
       }
     };
     scope.image_class = function(image) {
-      var isLarge = scope.imageLarge[image] || false;
+      var isLarge = scope.cv.image_large[image] || false;
       return isLarge ? "fr-img-lg" : "fr-img-sm";
     }
     scope.image_click = function(image) {
-      var isLarge = scope.imageLarge[image] || false;
-      scope.imageLarge[image] = !isLarge;
+      var isLarge = scope.cv.image_large[image] || false;
+      scope.cv.image_large[image] = !isLarge;
     };
     scope.image_GET = function(image) {
-      scope.image_instances[image] = Math.floor(Math.random()*1000000) ;
+      scope.cv.image_instances[image] = Math.floor(Math.random()*1000000) ;
     };
     scope.image_GET_icon = function(image) {
       return scope.transmit_enabled && (image === "camera.jpg" || image === 'monitor.jpg') ?
         "glyphicon glyphicon-repeat" : "";
     }
     
-    scope.show_resources = ['save', 'process'];
     scope.resource_text = function(resource) {
-      return scope.resource_response[resource] || "...";
+      return scope.resource_response[resource] || " ";
     }
     scope.resource_path = function(resource) {
-      return "/cv/" + scope.camera + "/" + scope.profile + "/cve/" + scope.cve + "/" + resource + ".json";
+      return "/cv/" + scope.cv.camera_name + "/" + scope.cv.profile_name + "/cve/" + scope.cv.cve_name + "/" + resource + ".json";
     };
     scope.resource_url = function(resource) {
-      return scope.camera_url() + scope.profile + "/cve/" + scope.cve + "/" + resource + ".json";
+      return scope.camera_url() + scope.cv.profile_name + "/cve/" + scope.cv.cve_name + "/" + resource + ".json";
     };
     scope.resource_class = function(resource) {
       return scope.resource_classname[resource] || "fr-json-ok";
@@ -172,10 +158,10 @@ controllers.controller('MainCtrl', ['$scope','$location', 'BackgroundThread',
 	scope.resource_response[resource] = response;
 	scope.resource_classname[resource] = classname;
 	var t = Math.floor(Math.random()*1000000) ;
-	scope.image_instances['camera.jpg'] = t;
-	scope.image_instances['monitor.jpg'] = t;
-	resource === 'save' && (scope.image_instances['saved.png'] = t);
-	resource === 'process' && (scope.image_instances['output.jpg'] = t);
+	scope.cv.image_instances['camera.jpg'] = t;
+	scope.cv.image_instances['monitor.jpg'] = t;
+	resource === 'save' && (scope.cv.image_instances['saved.png'] = t);
+	resource === 'process' && (scope.cv.image_instances['output.jpg'] = t);
 	scope.transmit_end(true);
       });
     }
@@ -200,11 +186,11 @@ controllers.controller('MainCtrl', ['$scope','$location', 'BackgroundThread',
     bg.worker = function(ticks) {
      if (scope.transmit_isIdle() && scope.transmit_enabled) {
        if ((ticks % 5) === 0 ) {
-	 scope.resource_update && scope.show_resources.indexOf('process') >= 0 && scope.resource_GET('process');
+	 scope.cv.resources.indexOf('process') >= 0 && scope.resource_GET('process');
        } else if ((ticks % 3) === 0 ) {
-	 scope.image_update && scope.image_GET('monitor.jpg');
+	 scope.image_GET('monitor.jpg');
        } else if ((ticks % 3) === 1 ) {
-	 scope.image_update && scope.image_GET('camera.jpg');
+	 scope.image_GET('camera.jpg');
        }
      }
      return true;
