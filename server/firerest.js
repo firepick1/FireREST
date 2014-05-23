1131c06f0e214efa03983176f6358bd131f9db37
firepick1 (Chromebook Ubuntu) <karl@firepick.org>
2014-05-23 11:35:27 -0700

    FireFUSE bug fix

--- server/firerest.js
+++ server/firerest.js
@@ -9,9 +9,16 @@ app.all('*', function(req, res, next) {
   next();
 });
 
-var cv_dir = "/dev/firefuse/cv";
+// By default, use reference implementation
+var __appdir = "www";
+app.use(express.static(__appdir));
+console.log("Mapping / to: " + __appdir);
+app.use(express.bodyParser());
+
+// If possible, use FireFUSE
 console.log("Looking for FireFUSE...");
-if (fs.existsSync("/dev/firefuse/cv")) {
+var cv_dir = "/dev/firefuse/cv";
+if (fs.existsSync(cv_dir)) {
   app.use('/firerest/cv', express.static(cv_dir));
   app.get('/firerest/config.json', function(req,res) {
     res.sendfile('/dev/firefuse/config.json');
@@ -21,11 +28,6 @@ if (fs.existsSync("/dev/firefuse/cv")) {
 } else {
   console.log("FireFUSE is not available. FireREST is demo mode only" );
 }
-
-var __appdir = "www";
-app.use(express.static(__appdir));
-console.log("Mapping / to: " + __appdir);
-app.use(express.bodyParser());
 
 var firerest_port=8001;
 app.listen(firerest_port);
