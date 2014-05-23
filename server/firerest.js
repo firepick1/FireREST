06a96ffce3337ee1a4312430a5dda04fe50b1c79
firepick1 (Chromebook Ubuntu) <karl@firepick.org>
2014-05-23 07:49:10 -0700

    remove deprecated content_source api

--- server/firerest.js
+++ server/firerest.js
@@ -2,18 +2,11 @@ console.log("loading express...");
 var express = require('express');
 var fs = require('fs');
 var app = express();
-var content_source = "Unknown";
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
-
-app.get("/firerest/content_source", function(req, res){
-  res.setHeader('Content-Type', 'text/plain');
-  res.send(content_source);
-  //console.log("content_source: " + content_source);
 });
 
 var firefuse_dir = "/dev/firefuse";
@@ -22,9 +15,7 @@ if (fs.existsSync("/dev/firefuse/cv/1/camera.jpg")) {
   app.use('/firerest', express.static(firefuse_dir));
   console.log("Found FireFUSE!");
   console.log("Mapping /firerest to: " + firefuse_dir);
-  content_source = "FireFUSE";
 } else {
-  content_source = "Demo";
   console.log("FireFUSE is not available. FireREST is demo mode only" );
 }
 
