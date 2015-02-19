15e5ec99d063ce4f8b3b0979952d7abb21af78dd
firepick1 (pixel) <karl@firepick.org>
2015-02-19 07:23:07 -0800

    Evolve

--- www/js/Evolve.js
+++ www/js/Evolve.js
@@ -1,5 +1,5 @@
 var firepick; (function (firepick) {
-	var mutateDefault = function(value,minValue,maxValue) {
+	function mutateDefault(value,minValue,maxValue) {
 		// generate new value in given range with median==value 
 		// using approximately Gaussian distribution
 		assert.ok(minValue <= value);
@@ -16,24 +16,44 @@ var firepick; (function (firepick) {
 		assert.ok(result <= maxValue);
 		return result;
 	}
-	var Evolve = function(generate,compare) {
+	function Evolve(generate,compare) {
 		assert.ok(generate);	
 		this.generate = generate;
 		this.compare = compare;
 		this.mutate = mutateDefault;
+		this.nElite = 1;
+		this.nSurvivors = 20;
 		this.generation = [];
 		return this;
 	};
+	Evolve.prototype.withMutate = function(mutate)  {
+		assert.ok(mutate instanceof Function);
+		this.mutate = mutate;
+		return this;
+	};
+	Evolve.prototype.withElite = function(nElite) {
+		assert.ok(nElite >= 1);
+		this.nElite = nElite;
+		return this;
+	};
+	Evolve.prototype.withSurvivors = function(nSurvivors) {
+		assert.ok(nSurvivors >= 1);
+		this.nSurvivors = nSurvivors;
+		return this;
+	}
 	Evolve.prototype.evolve1 = function(generation) {
 		var generation1 = generation ? generation : this.generation;
-		var generation2 = [];
+		var generation2 = [];	
+		for (var i=0; i < Math.min(generation.length, this.nElite); i++) {
+			generation2.push(generation1[i]);
+		}
 		var variantMap = {};
 		for (var iv1=0; iv1 < generation1.length; iv1++) {
 			var v = generation1[iv1];
 			var iv2 = Math.round(Math.random() * 7919) % generation.length;
 			var parent1 = generation[Math.min(iv1,iv2)];
 			var parent2 = generation[Math.max(iv1,iv2)];
-			var candidates = this.generate(parent1, parent2);
+			var candidates = this.generate(parent1, parent2, mutateDefault);
 			for (var ic=0; ic < candidates.length; ic++) {
 				var c = candidates[ic];
 				var key = JSON.stringify(c);
@@ -48,14 +68,14 @@ var firepick; (function (firepick) {
 		}
 		return generation2;
 	};
-	Evolve.prototype.evolveN = function(n, generation, maxVariants) {
+	Evolve.prototype.solve = function(generation, isDone) {
 		this.generation = generation;
-		for (var i = 0; i < n; i++) {
+		assert.ok(isDone instanceof Function);
+		for (this.iGeneration = 0; (this.status=isDone(this.iGeneration, this.generation)) === false; this.iGeneration++) {
 			this.generation = this.evolve1(this.generation);
-			if (maxVariants && this.generation.length > maxVariants) {
-				this.generation.splice(maxVariants, this.generation.length);
+			if (this.nSurvivors && this.generation.length > this.nSurvivors) {
+				this.generation.splice(this.nSurvivors, this.generation.length);
 			}
-			//console.log("evolve"+(i+1) + JSON.stringify(this.generation));
 		}
 		return this.generation;
 	};
@@ -67,24 +87,35 @@ var firepick; (function (firepick) {
 var firepick; (function (firepick) {
 	var N = 200;
 	var N2 = N == 2 ? 2 : N/2
-	var generate = function(parent1, parent2) {
-		var kid1 = parent1;	// keep better solution 
-		var kid2;
+	
+	function isDone(n, generation) {
+		if (Math.abs(N - generation[0]*generation[0]) < N/1000) {
+			console.log("Solved in " + n + " generations: " + generation[0]);
+			return true;
+		}
+		if (n >= 100) {
+			console.log("Giving up after " + n + " generations");
+			return true;
+		}
+		return false;
+	};
+	function generate(parent1, parent2, mutate) {
+		var kids = [mutate(parent1, 1, N2)]; // broad search
 		if (parent1 === parent2) {
-			kid2 = evolve.mutate(parent1, 1, N2);
-		} else {
+			kids.push(mutate(parent1, 1, N2)); 
+		} else {	// deep search
 			var spread = Math.abs(parent1-parent2);
-			kid2 = evolve.mutate(parent1, Math.max(1, parent1-spread), Math.min(N2,parent1+spread));
+			kids.push(mutate(parent1, Math.max(1, parent1-spread), Math.min(N2,parent1+spread)));
 		}
 
-		return [kid1, kid2];
+		return kids;
 	};
-	var	compare = function(a,b) {
+	function compare(a,b) {
 		return Math.abs(N-a*a) - Math.abs(N-b*b);
 	};
-	var evolve = new firepick.Evolve(generate, compare);
 	var self = {
 		testAll: function(result, scope) {
+			var evolve = new firepick.Evolve(generate, compare);
 			assert.equal(1.5, 3/2);
 			assert.ok(compare(1.4,1) < 0);
 			assert.ok(compare(1.4,1.41) > 0);
@@ -92,13 +123,12 @@ var firepick; (function (firepick) {
 			var guess1 = (1+N2)/2;
 			console.log("guess1:" + guess1);
 			for (var i = 0; i < 10; i++) {
-				var vSolve = evolve.evolveN(30, [guess1], 20);
-				console.log("vSolve:" + JSON.stringify(vSolve));
+				var vSolve = evolve.withElite(1).withSurvivors(10).solve([guess1], isDone);
+				console.log("Last generation:" + JSON.stringify(vSolve));
 				var solution = vSolve[0];
+				assert.equal(true, evolve.status);
 				assert.ok(Math.abs(N - solution*solution) < N/1000);
 			}
-
-			console.log("The square root of " + N + " is " + solution);
 
 			result.outcome = true;
 		}
