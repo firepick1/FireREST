0b6103a60c3e312d7c147da92550220d859f0c28
firepick1 (pixel) <karl@firepick.org>
2015-02-26 11:17:19 -0800

    FireFUSEMarlin

---
+++ server/firepick/FireFUSEMarlin.js
@@ -0,0 +1,67 @@
+var should = require("should"),
+    module = module || {},
+    firepick;
+var fs = require('fs');
+	
+var firefuse_path = "/dev/firefuse/sync/cnc/marlin/gcode.fire";
+
+(function(firepick) {
+    function FireFUSEMarlin(path) {
+		this.path = path || firefuse_path;
+		try { 
+			this.stat = fs.statSync(this.path);
+			this.fd = fs.openSync(this.path, 'rs+');
+		} catch (err) {
+			this.err = err;
+		}
+        return this;
+    };
+    FireFUSEMarlin.prototype.isAvailable = function() {
+		return typeof this.err === 'undefined';
+	};
+    FireFUSEMarlin.prototype.write = function(data) {
+        if (this.err) {
+			throw {error:"open failed", cause:this.err};
+		}
+		fs.write(fd, data);
+    };
+
+    console.log("LOADED	: firepick.FireFUSEMarlin");
+    module.exports = firepick.FireFUSEMarlin = FireFUSEMarlin;
+})(firepick || (firepick = {}));
+
+(typeof describe === 'function') && describe("firepick.FireFUSEMarlin test", function() {
+    var ffm;
+	var ffm_bad;
+	it("should be creatable", function() {
+		should.exist(firepick, "firepick exists");
+		should.exist(firepick.FireFUSEMarlin, "firepick.FireFUSEMarlin exists");
+		should.doesNotThrow((function(){
+			ffm = new firepick.FireFUSEMarlin();
+		}));
+		should.exist(ffm, "FireFUSEMarlin");
+		should.doesNotThrow((function(){
+			ffm_bad = new firepick.FireFUSEMarlin("no/such/path")
+		}), "no/such/path");
+	});
+    it("should have Marlin path", function() {
+        should(ffm.path).equal(firefuse_path,  'Marlin path');
+        should(ffm_bad.path).equal("no/such/path",  'Marlin path');
+    });
+	it("should throw error when writing without Marlin", function() {
+		should.throws((function(){
+			ffm_bad.write("G28");
+		}));
+    });
+	if (new firepick.FireFUSEMarlin().isAvailable()) {
+		it("should be able to access Marlin via FireFUSE", function() {
+			should.equal(ffm.isAvailable(), true, "Marlin unavailable:" + ffm.path);
+		});
+		it("should home ", function() {
+			should.ok(ffm.write("G28"));
+		});
+	} else {
+		console.log("WARNING	: FireFUSE Marlin is unavailable (test skipped)");
+	}
+
+});
