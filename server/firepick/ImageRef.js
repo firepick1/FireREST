296d576ee4c3671d5718263c7f5ff7f6ce010a9b
firepick1 (pixel) <karl@firepick.org>
2015-02-27 10:33:01 -0800

    ImageRef

---
+++ server/firepick/ImageRef.js
@@ -0,0 +1,106 @@
+var should = require("should"),
+    module = module || {},
+    firepick;
+var child_process = require('child_process');
+var fs = require('fs');
+var temp = require('temp');
+temp.track();
+
+(function(firepick) {
+    function ImageRef(x,y,z,state,version) {
+        this.x = x || 0;
+		this.y = y || 0;
+		this.z = z || 0;
+		if (typeof state === 'string') { this.state = state; }
+		if (version) { this.version = version }
+        return this;
+    }
+	ImageRef.copy = function(refSrc) {
+		return new ImageRef(refSrc.x, refSrc.y, refSrc.z, refSrc.state, refSrc.version);
+	}
+	ImageRef.prototype.copy = function() {
+		return ImageRef.copy(this);
+	}
+    ImageRef.prototype.setVersion = function(version) {
+		this.version = version || 0;
+		return this;
+    }
+    ImageRef.prototype.setState = function(state) {
+		this.state = state;
+		return this;
+    }
+	ImageRef.nameOf = function(imgRef, prefix, suffix) {
+		var fname = (prefix || "") + "_" + imgRef.x + "_" + imgRef.y + "_" + imgRef.z;
+
+		if (imgRef.state || imgRef.version) {
+			fname += "#" + (imgRef.state || "");
+		}
+		if (imgRef.version) {
+			fname += "_" + imgRef.version;
+		}
+		return fname + (suffix || "");
+	}
+	ImageRef.prototype.name = function(prefix, suffix) {
+		return ImageRef.nameOf(this, prefix, suffix);
+	}
+	ImageRef.prototype.compare = function(that) {
+		return ImageRef.compare(this, that);
+	}
+	ImageRef.compare = function(img1,img2) {
+		var cmp = 
+			((img1.x||0) - (img2.x||0)) ||
+			((img1.y||0) - (img2.y||0)) ||
+			((img1.z||0) - (img2.z||0));
+		if (cmp === 0) {
+			var s1 = img1.state || "";
+			var s2 = img2.state || "";
+			cmp = (s1 === s2 ? 0 : (s1 < s2 ? -1 : 1));
+		}
+		cmp = cmp || ((img1.version || 0) - (img2.version || 0));
+		return cmp;
+	}
+
+    console.log("LOADED	: firepick.ImageRef");
+    module.exports = firepick.ImageRef = ImageRef;
+})(firepick || (firepick = {}));
+
+(typeof describe === 'function') && describe("firepick.ImageRef test", function() {
+    var ref000 = new firepick.ImageRef();
+	var ref123 = new firepick.ImageRef(1,2,3);
+
+    it("should be created", function() {
+        should.equal(0, ref000.x);
+        should.equal(0, ref000.y);
+        should.equal(0, ref000.z);
+        should.equal(1, ref123.x);
+        should.equal(2, ref123.y);
+        should.equal(3, ref123.z);
+	});
+    it("should be comparable", function() {
+        should.equal(0, firepick.ImageRef.compare({x:0,y:0,z:0},ref000));
+        should.equal(0, firepick.ImageRef.compare({z:3,y:2,x:1},ref123));
+        should(firepick.ImageRef.compare(ref000,ref123)).be.below(0);
+        should(firepick.ImageRef.compare(ref123,ref000)).be.above(0);
+        should(firepick.ImageRef.compare({x:0,y:0,z:0},{x:0,y:0,z:3})).be.below(0);
+		should(ref000.compare(ref123)).be.below(0);
+    });
+	it("should copy", function() {
+		var ref123a4 = new firepick.ImageRef(1,2,3,"a",4);
+		var ref123a4_copy1 = ref123a4.copy();
+		var ref123a4_copy2 = firepick.ImageRef.copy(ref123a4);
+		should.equal(0, ref123a4.compare(ref123a4_copy1));
+		should.equal(0, ref123a4.compare(ref123a4_copy2));
+		should.equal(0, ref123a4_copy1.compare(ref123a4_copy2));
+		should.equal(0, ref123a4_copy1.compare({version:4,state:"a",z:3,y:2,x:1}));
+	});
+	it("should have a name", function() {
+		var ref123a4 = new firepick.ImageRef(1,2,3,"a",4);
+		var ref123_4 = ref123.copy().setVersion(4);
+		var ref123b = ref123.copy().setState('b');
+		should.equal("_1_2_3", ref123.name());
+		should.equal("prefix_1_2_3.jpg", ref123.name("prefix",".jpg"));
+		should.equal("prefix_1_2_3#a_4.jpg", ref123a4.name("prefix",".jpg"));
+		should.equal("_1_2_3#_4.jpg", ref123_4.name("",".jpg"));
+		should.equal("prefix_1_2_3#b", ref123b.name("prefix"));
+	});
+});
--- server/firepick/XYZCamera.js
+++ server/firepick/XYZCamera.js
@@ -7,6 +7,7 @@ var os = require("os"),
 	path = require("path");
 firepick.XYZPositioner = require("./XYZPositioner");
 firepick.MockCamera = require("./MockCamera");
+firepick.ImageRef = require("./ImageRef");
 
 function isNumeric(obj) {
     return (obj - parseFloat(obj) + 1) >= 0;
@@ -16,36 +17,72 @@ function isNumeric(obj) {
     function XYZCamera(xyzPositioner, camera) {
         this.xyz = xyzPositioner || new firepick.XYZPositioner();
 		should.ok(firepick.XYZPositioner.validate(this.xyz, "XYZCamera(xyzPositioner)"));
-		this.camera = camera || new firepick.MockCamera();;
+		this.camera = camera || new firepick.MockCamera();
         return this;
     }
 	XYZCamera.prototype.home = function() { this.xyz.home(); return this; };
-	XYZCamera.prototype.origin = function() { this.xyz.origin(); return this; };
-	XYZCamera.prototype.move = function(path) { this.xyz.move(path); return this; };
+	XYZCamera.prototype.origin = function() { 
+		this.xyz.origin(); 
+		this.imgRef = firepick.ImageRef.copy(this.xyz.position());
+		return this; 
+	};
+	XYZCamera.prototype.move = function(path) { 
+		this.xyz.move(path); 
+		this.imgRef = firepick.ImageRef.copy(this.xyz.position());
+		return this; 
+	};
 	XYZCamera.prototype.position = function(path) { return this.xyz.position(); };
 	XYZCamera.prototype.isAvailable = function() { 
 		return this.xyz.isAvailable && this.xyz.isAvailable(); 
 	};
-	XYZCamera.prototype.xyzPath = function(x,y,z) {
-		return path.join(os.tmpDir(),"XYZCamera_" + x + "_" + y + "_" + z + ".jpg");
+	XYZCamera.prototype.pathOf = function(imgRef) {
+		return path.join(os.tmpDir(),imgRef.name("XYZCamera", ".jpg"));
 	};
-	XYZCamera.prototype.captureXYZ = function() {
-		var pos = this.xyz.position();
-		should.ok(isNumeric(pos.x));
-		should.ok(isNumeric(pos.y));
-		should.ok(isNumeric(pos.z));
-		var fpath = this.xyzPath(pos.x, pos.y, pos.z);
+	XYZCamera.prototype.imageRefOf = function(path) {
+		var $tokens = path.split('#');
+		var _tokens = $tokens[0].split('_');
+		if ($tokens.length > 1) {
+			var _tokens1 = $tokens[1].split('_');
+			_tokens.push(_tokens1[0],_tokens1[1]);
+		}
+		return new firepick.ImageRef(
+			_tokens[1]+0, /* x */
+			_tokens[2]+0, /* y */
+			_tokens[3]+0, /* z */
+			_tokens[4], /* state */
+			_tokens[5]); /* version */
+	};
+	XYZCamera.prototype.pathOf = function(imgRef) {
+		var fname = firepick.ImageRef.nameOf(imgRef, "XYZCamera", ".jpg");
+		return path.join(os.tmpDir(),fname);
+	}
+
+	XYZCamera.prototype.xyzPath = function(x,y,z,state,version) {
+		x = (typeof x === 'undefined' || x === null) ? this.imgRef.x : x;
+		y = (typeof y === 'undefined' || y === null) ? this.imgRef.y : y;
+		z = (typeof z === 'undefined' || z === null) ? this.imgRef.z : z;
+		var imgRef = new firepick.ImageRef(x,y,z,state,version);
+		return path.join(os.tmpDir(),imgRef.name("XYZCamera", ".jpg"));
+	};
+	XYZCamera.prototype.captureSave = function(state, version) {
+		var imgRef = this.imgRef.copy().setState(state).setVersion(version);
+		var fpath = this.pathOf(imgRef);
 		this.camera.capture();
 		fs.writeFileSync(fpath, fs.readFileSync(this.camera.path));
-		return this;
+		return imgRef;
 	};
-	XYZCamera.prototype.calcOffset = function(x,y,z) {
-		this.camera.capture();
-		var fname = this.xyzPath(x, y, z);
+	XYZCamera.prototype.calcOffset = function(imgRef1, imgRef2) {
+		should.exist(imgRef1);
+		var fname1 = this.camera.path;
+		var fname2 = this.pathOf(imgRef1);
+		if (typeof imgRef2 !== 'undefined') {
+			fname1 = fname2;
+			fname2 = this.pathOf(imgRef2);
+		}
 		var cmd = "firesight" +
-			" -i " + this.camera.path + 
+			" -i " + fname1 + 
 			" -p server/json/calcOffset.json" +
-			" -Dtemplate=" + fname;
+			" -Dtemplate=" + fname2;
 		//console.log(cmd);
 		var out = child_process.execSync(cmd);
 		var jout = JSON.parse(out.toString());
@@ -68,7 +105,8 @@ function isNumeric(obj) {
 		this.timeout(5000);
 		camera.push("test/camX0Y0Z0a.jpg");
 		should.deepEqual({x:0,y:0,z:0}, xyzCam.origin().position());
-		should.equal(xyzCam, xyzCam.captureXYZ());
+		var ref000 = xyzCam.captureSave();
+		should.equal(0, firepick.ImageRef.compare({x:0,y:0,z:0}, ref000));
 		should.deepEqual({x:0,y:0,z:0},xyzCam.position());
 		var camX0Y0Z0a = fs.statSync("test/camX0Y0Z0a.jpg");
 		var x0y0z0_1 = fs.statSync(xyzCam.xyzPath(0,0,0));
@@ -77,7 +115,8 @@ function isNumeric(obj) {
 	it("should take a different picture at (0,0,0)", function() {
 		camera.push("test/camX0Y0Z0b.jpg");
 		should.deepEqual({x:0,y:0,z:0}, xyzCam.origin().position());
-		should.equal(xyzCam, xyzCam.captureXYZ());
+		var ref000 = xyzCam.captureSave();
+		should.equal(0, firepick.ImageRef.compare({x:0,y:0,z:0}, ref000));
 		var camX0Y0Z0b = fs.statSync("test/camX0Y0Z0b.jpg");
 		var x0y0z0_2 = fs.statSync(xyzCam.xyzPath(0,0,0));
 		should.deepEqual(camX0Y0Z0b.size, x0y0z0_2.size);
@@ -88,14 +127,25 @@ function isNumeric(obj) {
 		var fx1y0z0 = xyzCam.xyzPath(1,0,0);
 		fs.writeFileSync(fx1y0z0, "");
 		should.deepEqual({x:1,y:0,z:0}, xyzCam.move({x:1,y:0,z:0}).position());
-		should.equal(xyzCam, xyzCam.captureXYZ(1,0,0));
+		var ref100 = xyzCam.captureSave();
+		should.equal(0, firepick.ImageRef.compare({x:1,y:0,z:0}, ref100));
 		should.deepEqual({x:1,y:0,z:0},xyzCam.position());
 		x1y0z0 = fs.statSync(fx1y0z0);
 		should.equal(camX1Y0Z0.size, x1y0z0.size);
 	});
-	it("should calculate the image offset with respect to another XYZ", function() {
+	it("should calculate the current image offset with respect to another XYZ", function() {
 		camera.push("test/camX0Y0Z0a.jpg");
-		var channels = xyzCam.origin().calcOffset(0,0,0);
+		xyzCam.captureSave();
+		var channels = xyzCam.origin().calcOffset({x:0,y:0,z:0});
 		should.deepEqual({dx:0, dy:0, match:"0.995454"}, channels["0"]);
+	});
+	it("should calculate the image offset of two saved images", function() {
+		camera.push("test/camX0Y0Z0b.jpg");
+		camera.push("test/camX1Y0Z0.jpg");
+		var ref000_test1 = xyzCam.captureSave("eagle",1);
+		xyzCam.move({x:1});
+		var ref100_test1 = xyzCam.captureSave("hawk",1);
+		var channels = xyzCam.origin().calcOffset(ref000_test1, ref100_test1);
+		should.deepEqual({dx:-5, dy:1, match:"0.916843"}, channels["0"]);
 	});
 })
