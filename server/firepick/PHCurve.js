9a06074378cabf9cbb99068eb427a74a1066a6aa
firepick1 (pixel) <karl@firepick.org>
2015-03-29 07:47:57 -0700

    refactor

--- server/firepick/PHCurve.js
+++ server/firepick/PHCurve.js
@@ -6,6 +6,8 @@ Bernstein = require("./Bernstein");
 Tridiagonal = require("./Tridiagonal");
 
 (function(firepick) {
+	var b4 = new Bernstein(4);
+	var b5 = new Bernstein(5);
     function PHCurve(pts,options) {
 		var that = this;
 		options = options || {};
@@ -18,9 +20,6 @@ Tridiagonal = require("./Tridiagonal");
 		pts.length.should.be.above(1);
 		initz(that,pts);
 		that.N = that.q.length-1;
-		that.b4 = new Bernstein(4);
-		that.b5 = new Bernstein(5);
-		that.b6 = new Bernstein(6);
 		var u = [0,0,0];
 		var v = [0,0,0];
 		that.sigmak = [
@@ -94,7 +93,7 @@ Tridiagonal = require("./Tridiagonal");
 		var that = this;
 		var sum = 0;
 		for (var k=0; k <= that.degree; k++) {
-			var b5c = that.b5.coefficient(k, tau);
+			var b5c = b5.coefficient(k, tau);
 			sum += that.sik(i,k) * b5c;
 			that.logger.trace("sit k:", k, " sum:", sum, " b5c:", b5c, " tau:", tau);
 		}
@@ -305,59 +304,6 @@ Tridiagonal = require("./Tridiagonal");
 		default: should.fail("invalid k:" + k);
 		}
 	};
-	PHCurve.prototype.Vk = function(vin,vout,k) {
-		var that = this;
-		return k < that.degree2 ? vin : vout;
-	};
-	PHCurve.prototype.V = function(vin, vout, t) {
-		var that = this;
-		var sum = 0;
-		for (var k=0; k <= that.degree; k++) {
-			that.logger.trace("that.degree:", that.degree, " k:", k, " t:", t,
-				" V(k):", that.Vk(vin,vout,k), 
-				" coefficient(k,t):", that.b5.coefficient(k,t));
-			sum += that.Vk(vin,vout,k) * that.b5.coefficient(k,t);
-		}
-		return sum/(1+that.degree);
-	};
-	PHCurve.prototype.Fk = function(vin, vout, k) {
-		var that = this;
-		var sum = 0;
-		for (var j=0; j < k; j++) {
-			sum += that.Vk(vin,vout,j);
-		}
-		return sum/(1+that.degree);
-	};
-	PHCurve.prototype.F = function(t) {
-		var that = this;
-		return that.Fvt(2,2,t);
-	};
-	PHCurve.prototype.Fvt = function(vin, vout, t, T) {
-		var that = this;
-		var sum = 0;
-		var n1 = that.degree+1;
-		T = T || 1;
-		for (var k=0; k <= n1; k++) {
-			sum += that.Fk(vin,vout,k) * that.b6.coefficient(k,t);
-		}
-		return T * sum;
-	};
-	PHCurve.prototype.xyi = function(i,t) {
-		var that = this;
-		var sum = 0;
-		var t1 = 1-t;
-		var tk = [1];
-		var t1k = [1];
-		for (var k=1; k<=that.degree; k++) {
-			tk.push(t*tk[k-1]);
-			t1k.splice(0, 0, t1*t1k[0]);
-		}
-		for (var k=0; k<=5; k++) {
-			sum += that.p[i][k] * Util.choose(5,k) * t1k[k] * tk[k];
-		}
-
-		return sum;
-	};
 	PHCurve.prototype.sigmat = function(t) {
 		var that = this;
 		var sum = 0;
@@ -498,38 +444,6 @@ Tridiagonal = require("./Tridiagonal");
 	it("new PHCurve(5,[pts]) should create a 5-degree PHCurve instance", function() {
 		var ph5 = new PHCurve(pts);
 		ph5.should.have.properties({degree:5,degree2:3});
-	});
-	it("V(vin,vout,t) should interpolate velocity on interval t:[0,1]", function() {
-		var ph5 = new PHCurve(pts,{logger:logger});
-		/*
-		ph5.V(-100,100,0).should.equal(-100);
-		ph5.V(-100,100,0.1).should.within(-84,-83);
-		ph5.V(-100,100,0.2).should.within(-65,-64);
-		ph5.V(-100,100,0.3).should.within(-45,-44);
-		ph5.V(-100,100,0.4).should.within(-23,-22);
-		ph5.V(-100,100,0.5).should.within(0,0);
-		ph5.V(-100,100,0.6).should.within(22,23);
-		ph5.V(-100,100,0.7).should.within(44,45);
-		ph5.V(-100,100,0.8).should.within(64,65);
-		ph5.V(-100,100,0.9).should.within(83,84);
-		ph5.V(-100,100,1).should.equal(100);
-		*/
-	});
-	it("Fvt(vin,vout,t) should interpolate V integral on interval t:[0,1]", function() {
-		var ph5 = new PHCurve(pts,{logger:logger});
-		/*
-		ph5.Fvt(-100,100,0).should.equal(-100);
-		ph5.Fvt(-100,100,0.1).should.within(-111,-110);
-		ph5.Fvt(-100,100,0.2).should.within(-122,-121);
-		ph5.Fvt(-100,100,0.3).should.within(-130,-129);
-		ph5.Fvt(-100,100,0.4).should.within(-136,-135);
-		ph5.Fvt(-100,100,0.5).should.within(-138,-137);
-		ph5.Fvt(-100,100,0.6).should.within(-136,-135);
-		ph5.Fvt(-100,100,0.7).should.within(-130,-129);
-		ph5.Fvt(-100,100,0.8).should.within(-122,-121);
-		ph5.Fvt(-100,100,0.9).should.within(-111,-110);
-		ph5.Fvt(-100,100,1).should.equal(-100);
-		*/
 	});
 	it("new PHCurve([p1,p2]).r(tau) should interpolate a 2-point straight line", function() {
 		var ph = new PHCurve([
