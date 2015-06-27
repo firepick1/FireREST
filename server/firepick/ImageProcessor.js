ae6851af626a68b5cdfc46d625851f4bc1c5a0e8
firepick1 (localhost) <karl@firepick.org>
2015-06-27 12:50:00 -0700

    measureGrid

--- server/firepick/ImageProcessor.js
+++ server/firepick/ImageProcessor.js
@@ -1,10 +1,10 @@
 var should = require("should"),
     module = module || {},
-    child_process = require("child_process"),
     firepick = firepick || {};
 var fs = require("fs");
 var path = require("path");
 var os = require("os");
+var child_process = require("child_process");
 Camera = require("./Camera");
 ImageRef = require("./ImageRef");
 ImageStore = require("./ImageStore");
@@ -25,10 +25,15 @@ Logger = require("./Logger");
         if (args) {
             cmd += " " + args;
         }
-        var out = child_process.execSync(cmd);
-        var jout = JSON.parse(out.toString());
-		logger.trace(cmd, " => ", jout);
-        return jout;
+		try {
+			var out = child_process.execSync(cmd);
+			var jout = JSON.parse(out.toString());
+			logger.trace(cmd, " => ", jout);
+			return jout;
+		} catch(err) {
+			logger.info("ERROR:", err);
+			return err;
+		}
     };
     ImageProcessor.prototype.health = function() {
         return 1;
@@ -52,6 +57,19 @@ Logger = require("./Logger");
     ImageProcessor.prototype.sharpness = function(imgRef) {
 		var that = this;
         return that.firesight_cmd(imgRef.path, "sharpness.json").result;
+    };
+    ImageProcessor.prototype.measureGrid = function(imgRef) {
+		var that = this;
+        var cmdResult = that.firesight_cmd(imgRef.path, "measureGrid.json",
+			"-Dtemplate=server/img/cross32.png").result;
+		var x = cmdResult["grid.x"] || null;
+		var y = cmdResult["grid.y"] || null;
+		var result = {grid:{}}
+		x && (result.grid.x = x);
+		if (x) { result.grid.x = x; }
+		if (y) { result.grid.y = y; }
+		
+		return result;
     };
     ImageProcessor.prototype.PSNR = function(imgRef1, imgRef2) {
 		var that = this;
@@ -151,4 +169,90 @@ Logger = require("./Logger");
 (typeof describe === 'function') && describe("firepick.ImageProcessor test", function() {
     var ip = new firepick.ImageProcessor();
     firepick.ImageProcessor.validate(ip);
-})
+
+	var ref000a = {
+		x: 0,
+		y: 0,
+		z: 0,
+		path: "test/camX0Y0Z0a.jpg"
+	};
+	var ref000b = {
+		x: 0,
+		y: 0,
+		z: 0,
+		path: "test/camX0Y0Z0b.jpg"
+	};
+	var ref100 = {
+		x: 0,
+		y: 0,
+		z: 0,
+		path: "test/camX1Y0Z0.jpg"
+	};
+	var refDuck = {
+		x: 0,
+		y: 0,
+		z: 0,
+		path: "test/duck.jpg"
+	};
+	var refZ_40 = {
+		x: 0,
+		y: 0,
+		z: -40,
+		path: "test/XP005_Z-040X0Y0@1#1.jpg"
+	};
+	var refZ_80 = {
+		x: 0,
+		y: 0,
+		z: -80,
+		path: "test/XP005_Z-080X0Y0@1#1.jpg"
+	};
+	it("should show zero offset for two images at same location", function() {
+		var result = ip.calcOffset(ref000a, ref000b);
+		should.exist(result);
+		should.exist(result[0]);
+		should.equal(result[0].dx, 0);
+		should.equal(result[0].dy, 0);
+	});
+	it("should show non-zero offset for two images at different location", function() {
+		var result = ip.calcOffset(ref000a, ref100);
+		should.exist(result);
+		should.exist(result[0]);
+		should(result[0].dx * result[0].dx).above(10);
+		should(result[0].dy * result[0].dy).below(5);
+	});
+	it("should return nothing for dissimilar images", function() {
+		var result = ip.calcOffset(ref000a, refDuck);
+		should.not.exist(result);
+	});
+	it("TESTTESTshould calculate the mean and standard deviation of an image", function() {
+		var result = ip.meanStdDev(ref000a);
+		//console.log(JSON.stringify(result));
+		should.deepEqual(result, {
+			"mean": [252.663, 254.641, 251.631, 0.0],
+			"stdDev": [2.11665, 1.92387, 9.66363, 0.0]
+		});
+	});
+	it("sharpness(imgRef) should calculate the sharpness of an image", function() {
+		var result = ip.sharpness(ref000a);
+		should.deepEqual(result, {
+			"sharpness": 3.05462
+		});
+	});
+	it("TESTTESTPSNR(imgRef1,imgRef2) should calculate the Power Signal to Noise Ratio of two images", function() {
+		var result = ip.PSNR(ref000a, ref000b);
+		should.deepEqual(result, {
+			"PSNR": 52.5422
+		});
+	});
+	it("TESTTESTmeasureGrid(imgRef1,imgRef2) should measure grid line separation", function() {
+		should.deepEqual(ip.measureGrid(refZ_40), {
+			"grid":{
+				x:8.79561, 
+				y:8.846
+			}
+		});
+		should.deepEqual(ip.measureGrid(refZ_80), {
+			"grid":{}
+		});
+	});
+});
